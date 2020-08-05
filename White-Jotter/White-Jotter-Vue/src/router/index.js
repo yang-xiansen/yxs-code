bdf0bacbb80af93da889cf62eacd07ff9088dcd4
杨-先森 <271819776@qq.com>
2020-08-06 07:29:58 +0800

    白卷初始化

--- White-Jotter/White-Jotter-Vue/src/router/index.js
+++ White-Jotter/White-Jotter-Vue/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// 导入刚才编写的组件
+import appIndex from '@/components/home/appIndex'
+import login from '@/components/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // 下面都是固定的写法
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/index',
+      name: 'appIndex',
+      component: appIndex
     }
   ]
 })
+
