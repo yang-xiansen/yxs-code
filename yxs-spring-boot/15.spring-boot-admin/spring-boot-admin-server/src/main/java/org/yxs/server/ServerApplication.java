fa928c0264fe2532471e9e510e8d4bb5773f583d
yang-xiansen <271819776@qq.com>
2020-08-19 20:02:39 +0800

    springboot发送admin

---
+++ yxs-spring-boot/15.spring-boot-admin/spring-boot-admin-server/src/main/java/org/yxs/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package org.yxs.server;
+
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
