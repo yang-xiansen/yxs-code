3a7b957aab2b2c34bd17c741d0aa347e46fdc59a
yang-xiansen <271819776@qq.com>
2020-08-19 20:00:51 +0800

    springboot整合actuator

---
+++ yxs-spring-boot/13.spring-boot-actuator/src/main/java/org/yxs/actuator/ActuatorApplication.java
@@ -0,0 +1,14 @@
+package org.yxs.actuator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
