1d2408f9c77af0001084bd1084a5ba4bedf0a8ca
yang-xiansen <271819776@qq.com>
2020-08-19 19:58:36 +0800

    springboot整合swagger2

---
+++ yxs-spring-boot/12.spring-boot-swagger2/src/main/java/org/yxs/swagger/controller/SwaggerController.java
@@ -0,0 +1,104 @@
+package org.yxs.swagger.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.yxs.swagger.entity.User;
+import springfox.documentation.annotations.ApiIgnore;
+
+/**
+* @Description: 访问地址：http://localhost:8080/swagger-ui.html
+* @Author: yang-xiansen
+* @Date: 2020/08/19 10:36
+*/
+@Api(value = "Swagger测试Controller")
+@Controller
+@RequestMapping("user")
+public class SwaggerController {
+
+    @ApiIgnore
+    @GetMapping("hello")
+    public @ResponseBody
+    String hello() {
+        return "hello";
+    }
+
+    @ApiOperation(value = "获取用户信息", notes = "根据用户id获取用户信息")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "Long", paramType = "path")
+    @GetMapping("/{id}")
+    public @ResponseBody
+    User getUserById(@PathVariable(value = "id") Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("yxs");
+        user.setAge(25);
+        return user;
+    }
+
+    @ApiOperation(value = "获取用户列表", notes = "获取用户列表")
+    @GetMapping("/list")
+    public @ResponseBody
+    List<User> getUserList() {
+        List<User> list = new ArrayList<>();
+        User user1 = new User();
+        user1.setId(1l);
+        user1.setName("yxs");
+        user1.setAge(25);
+        list.add(user1);
+        User user2 = new User();
+        user2.setId(2l);
+        user2.setName("scott");
+        user2.setAge(29);
+        list.add(user2);
+        return list;
+    }
+
+    @ApiOperation(value = "新增用户", notes = "根据用户实体创建用户")
+    @ApiImplicitParam(name = "user", value = "用户实体", required = true, dataType = "User")
+    @PostMapping("/add")
+    public @ResponseBody
+    Map<String, Object> addUser(@RequestBody User user) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", "success");
+        return map;
+    }
+
+    @ApiOperation(value = "删除用户", notes = "根据用户id删除用户")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "Long", paramType = "path")
+    @DeleteMapping("/{id}")
+    public @ResponseBody
+    Map<String, Object> deleteUser(@PathVariable(value = "id") Long id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", "success");
+        return map;
+    }
+
+    @ApiOperation(value = "更新用户", notes = "根据用户id更新用户")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "Long", paramType = "path"),
+        @ApiImplicitParam(name = "user", value = "用户实体", required = true, dataType = "User")})
+    @PutMapping("/{id}")
+    public @ResponseBody
+    Map<String, Object> updateUser(@PathVariable(value = "id") Long id, @RequestBody User user) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", "success");
+        return map;
+    }
+
+}
---
+++ yxs-spring-boot/12.spring-boot-swagger2/src/main/java/org/yxs/swagger/entity/User.java
@@ -0,0 +1,13 @@
+package org.yxs.swagger.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
