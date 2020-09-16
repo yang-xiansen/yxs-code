0c99458639e28be7f74e572f0ce17986d2875bba
yang-xiansen <271819776@qq.com>
2020-09-16 20:02:00 +0800

    shiro使用redis做缓存

---
+++ yxs-spring-shiro/03.spring-shiro-redis/src/main/java/org/yxs/shiro/config/ShiroConfig.java
@@ -0,0 +1,154 @@
+package org.yxs.shiro.config;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+
+@Configuration
+public class ShiroConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password:}")
+    private String password;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //设置securityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //登陆得url
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        //登陆成功后跳转得url
+        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //未授权url
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+
+
+        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+
+        // 定义filterChain，静态资源不拦截
+        filterChainDefinitionMap.put("/css/**", "anon");
+        filterChainDefinitionMap.put("/js/**", "anon");
+        filterChainDefinitionMap.put("/fonts/**", "anon");
+        filterChainDefinitionMap.put("/img/**", "anon");
+        // druid数据源监控页面不拦截
+        filterChainDefinitionMap.put("/druid/**", "anon");
+        // 配置退出过滤器，其中具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/", "anon");
+        // 除上以外所有url都必须认证通过才可以访问，未通过认证自动访问LoginUrl
+        filterChainDefinitionMap.put("/**", "authc");
+
+        //user指的是用户认证通过或者配置了Remember Me记住用户登录状态后可访问。
+
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
+        // 配置SecurityManager，并注入shiroRealm
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(shiroRealm());
+
+        //设置记住我
+        securityManager.setRememberMeManager(rememberMeManager());
+
+        //使用redis
+        securityManager.setCacheManager(cacheManager());
+        return securityManager;
+    }
+
+
+    @Bean
+    public ShiroRealm shiroRealm() {
+        //配置Realm
+        ShiroRealm shiroRealm = new ShiroRealm();
+        return shiroRealm;
+    }
+
+
+    /**
+     * cookie对象
+     *
+     * @return
+     */
+    public SimpleCookie rememberMeCookie() {
+        // 设置cookie名称，对应login.html页面的<input type="checkbox" name="rememberMe"/>  类似security的记住我
+        SimpleCookie cookie = new SimpleCookie("rememberMe");
+        // 设置cookie的过期时间，单位为秒，这里为一天
+        cookie.setMaxAge(86400);
+        return cookie;
+    }
+
+    /**
+     * cookie管理对象
+     *
+     * @return
+     */
+    public CookieRememberMeManager rememberMeManager() {
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+        // rememberMe cookie加密的密钥
+        cookieRememberMeManager.setCipherKey(Base64.decode("4AvVhmFLUs0KTA3Kprsdag=="));
+        return cookieRememberMeManager;
+    }
+
+    /**
+     * @Description: 开启shiro权限注解
+     * @Author: y-xs
+     * @Date: 2020/09/15 8:57
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+
+    /**
+     * @Description: shiro配置redis
+     * @Author: y-xs
+     * @Date: 2020/09/15 14:18
+     */
+    public RedisManager redisManager() {
+        RedisManager redisManager = new RedisManager();
+        redisManager.setHost(host);
+        redisManager.setPort(port);
+        if (StringUtils.isNotBlank(password))
+            redisManager.setPassword(password);
+        redisManager.setTimeout(timeout);
+        return redisManager;
+    }
+
+    /**
+     * @Description: 缓存管理
+     * @Author: y-xs
+     * @Date: 2020/09/15 15:28
+     */
+    public RedisCacheManager cacheManager() {
+        RedisCacheManager redisCacheManager = new RedisCacheManager();
+        redisCacheManager.setRedisManager(redisManager());
+        return redisCacheManager;
+    }
+
+}
---
+++ yxs-spring-shiro/03.spring-shiro-redis/src/main/java/org/yxs/shiro/config/ShiroRealm.java
@@ -0,0 +1,92 @@
+package org.yxs.shiro.config;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.yxs.shiro.entity.Permission;
+import org.yxs.shiro.entity.Role;
+import org.yxs.shiro.entity.User;
+import org.yxs.shiro.mapper.UserPermissionMapper;
+import org.yxs.shiro.mapper.UserRoleMapper;
+import org.yxs.shiro.service.UserService;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ShiroRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+
+    @Autowired
+    private UserPermissionMapper userPermissionMapper;
+
+    /**
+     * 获取用户角色和权限
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        String userName = user.getUserName();
+
+        System.out.println("用户" + userName + "获取权限-----ShiroRealm.doGetAuthorizationInfo");
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+
+        // 获取用户角色集
+        List<Role> roleList = userRoleMapper.findByUserName(userName);
+        Set<String> roleSet = new HashSet<String>();
+        for (Role r : roleList) {
+            roleSet.add(r.getName());
+        }
+        simpleAuthorizationInfo.setRoles(roleSet);
+
+        // 获取用户权限集
+        List<Permission> permissionList = userPermissionMapper.findByUserName(userName);
+        Set<String> permissionSet = new HashSet<String>();
+        for (Permission p : permissionList) {
+            //获取权限
+            permissionSet.add(p.getName());
+        }
+        simpleAuthorizationInfo.setStringPermissions(permissionSet);
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 登陆认证
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //获取用户输入得用户名和密码
+        String username = (String) authenticationToken.getPrincipal();
+        String password = new String((char[]) authenticationToken.getCredentials());
+
+        // 通过用户名到数据库查询用户信息
+        User user = userService.findByUserName(username);
+
+        if (user == null) {
+            throw new UnknownAccountException("用户名或密码错误！");
+        }
+        if (!password.equals(user.getPassword())) {
+            throw new IncorrectCredentialsException("用户名或密码错误！");
+        }
+        if (user.getStatus().equals("0")) {
+            throw new LockedAccountException("账号已被锁定,请联系管理员！");
+        }
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
+        return info;
+    }
+}
