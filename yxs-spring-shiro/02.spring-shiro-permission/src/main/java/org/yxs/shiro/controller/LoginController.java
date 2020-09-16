7aaf2ed3ab9ebb3dc94b5a995d690ef6b927a9b7
yang-xiansen <271819776@qq.com>
2020-09-16 20:01:22 +0800

    shiro权限管理

---
+++ yxs-spring-shiro/02.spring-shiro-permission/src/main/java/org/yxs/shiro/controller/LoginController.java
@@ -0,0 +1,67 @@
+package org.yxs.shiro.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.yxs.shiro.core.Result;
+import org.yxs.shiro.entity.User;
+import org.yxs.shiro.util.MD5Utils;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public Result login(String username, String password, Boolean rememberMe) {
+        // 密码MD5加密
+        password = MD5Utils.encrypt(username, password);
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
+        // 获取Subject对象
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(token);
+            return Result.ok();
+        } catch (UnknownAccountException e) {
+            return Result.error(e.getMessage());
+        } catch (IncorrectCredentialsException e) {
+            return Result.error(e.getMessage());
+        } catch (LockedAccountException e) {
+            return Result.error(e.getMessage());
+        } catch (AuthenticationException e) {
+            return Result.error("认证失败！");
+        }
+    }
+
+    @RequestMapping("/")
+    public String redirectIndex() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        // 登录成后，即可通过Subject获取登录的用户信息
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        model.addAttribute("user", user);
+        return "index";
+    }
+
+    @GetMapping("/403")
+    public String forbid() {
+        return "403";
+    }
+}
