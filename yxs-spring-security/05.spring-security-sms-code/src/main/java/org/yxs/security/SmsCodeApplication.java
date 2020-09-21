568386d43f9bed15ffafcb424a5f3fed2227b80c
yang-xiansen <271819776@qq.com>
2020-09-21 21:16:29 +0800

    Spring Security短信验证码登录

---
+++ yxs-spring-security/05.spring-security-sms-code/src/main/java/org/yxs/security/SmsCodeApplication.java
@@ -0,0 +1,13 @@
+package org.yxs.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsCodeApplication.class, args);
+    }
+
+}
---
+++ yxs-spring-security/05.spring-security-sms-code/src/main/java/org/yxs/security/controller/SecurityController.java
@@ -0,0 +1,55 @@
+package org.yxs.security.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class SecurityController {
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+
+    @GetMapping("/authentication/require")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String requireAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        if (savedRequest != null) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            if (StringUtils.endsWithIgnoreCase(targetUrl, ".html"))
+                redirectStrategy.sendRedirect(request, response, "/login.html");
+        }
+        return "访问的资源需要身份认证";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    @GetMapping("index")
+    public Object index(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @GetMapping("index1")
+    public Object index1(Authentication authentication) {
+        return authentication;
+    }
+
+
+}
