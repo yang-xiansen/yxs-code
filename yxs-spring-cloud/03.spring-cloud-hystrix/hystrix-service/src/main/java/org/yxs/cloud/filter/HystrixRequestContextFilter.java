5568e1ed726cc0cd5beb4a77801080a753ed4acc
yang-xiansen <271819776@qq.com>
2020-10-10 20:11:17 +0800

    Hystrix服务容错保护

---
+++ yxs-spring-cloud/03.spring-cloud-hystrix/hystrix-service/src/main/java/org/yxs/cloud/filter/HystrixRequestContextFilter.java
@@ -0,0 +1,31 @@
+package org.yxs.cloud.filter;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+* @Description: 解决使用缓存出现的HystrixRequestContext异常
+* @Author: y-xs
+* @Date: 2020/10/09 10:25
+*/
+@Component
+@WebFilter(urlPatterns = "/*", asyncSupported = true)
+public class HystrixRequestContextFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            context.close();
+        }
+    }
+}
