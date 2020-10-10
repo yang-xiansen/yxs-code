251bbbbce909e26e4e412649b0045fc34a4eba30
yang-xiansen <271819776@qq.com>
2020-10-10 20:12:19 +0800

    OpenFeign基于Ribbon和Hystrix的声明式服务调用

---
+++ yxs-spring-cloud/05.spring-cloud-openfeign/feign-service/src/main/java/org/yxs/cloud/controller/UserFeignController.java
@@ -0,0 +1,50 @@
+package org.yxs.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.yxs.cloud.core.CommonResult;
+import org.yxs.cloud.domain.User;
+import org.yxs.cloud.feign.UserService;
+
+/**
+ * @Description: 远程调用user
+ * @Author: y-xs
+ * @Date: 2020/10/10 10:35
+ */
+@RestController
+@RequestMapping("/feign/user")
+public class UserFeignController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public CommonResult getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("/getByUsername")
+    public CommonResult getByUsername(@RequestParam String username) {
+        return userService.getByUsername(username);
+    }
+
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PostMapping("/update")
+    public CommonResult update(@RequestBody User user) {
+        return userService.update(user);
+    }
+
+    @PostMapping("/delete/{id}")
+    public CommonResult delete(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+}
