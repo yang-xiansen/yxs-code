4d16f31a7a780babd5f7de3a373a04548482118e
yang-xiansen <271819776@qq.com>
2020-10-12 08:10:12 +0800

    admin微服务应用监控

---
+++ yxs-spring-cloud/12.spring-boot-admin/admin-security-server/src/main/java/org/yxs/cloud/AdminSecurityServerApplication.java
@@ -0,0 +1,17 @@
+package org.yxs.cloud;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSecurityServerApplication.class, args);
+    }
+
+}
