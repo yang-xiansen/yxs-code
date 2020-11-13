38fc386624f1a4d14e5bac29988dfbc89b8c342b
yang-xiansen <271819776@qq.com>
2020-11-13 15:19:46 +0800

    SpringBoot跨域处理

---
+++ yxs-spring-boot/30.spring-boot-cors-support/src/main/java/org/yxs/cors/CorsApplication.java
@@ -0,0 +1,12 @@
+package org.yxs.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsApplication.class, args);
+    }
+}
