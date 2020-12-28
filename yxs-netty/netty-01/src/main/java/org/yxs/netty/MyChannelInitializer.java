e892c3d756087796fdc8c98d53e036ee10ef4e68
杨-先森 <271819776@qq.com>
2020-12-28 22:29:13 +0800

    netty入门

---
+++ yxs-netty/netty-01/src/main/java/org/yxs/netty/MyChannelInitializer.java
@@ -0,0 +1,15 @@
+package org.yxs.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+        System.out.println("链接开始，客户端链接");
+        System.out.println("链接报告Ip：" + socketChannel.localAddress().getHostString());
+        System.out.println("链接报告Port:" + socketChannel.localAddress().getPort());
+        System.out.println("链接报告完毕");
+    }
+}
