e10c2ad9b88927b7a10182a1d2907df15d5fab19
杨-先森 <271819776@qq.com>
2020-12-29 07:22:22 +0800

    NettyClient半包粘包处理、编码解码处理、收发数据方式

---
+++ yxs-netty/netty-08/src/main/java/org/yxs/netty/NettyClient.java
@@ -0,0 +1,44 @@
+package org.yxs.netty;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author: yang-xiansen
+ * @Date: 2020/12/28 21:32
+ * @Description: netty client
+ */
+public class NettyClient {
+
+    public static void main(String[] args) {
+        NettyClient nettyClient = new NettyClient();
+        nettyClient.connect("127.0.0.1", 9999);
+    }
+
+    private void connect(String host, int port) {
+        //配置服务的nio线程组
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workGroup)
+                .channel(NioSocketChannel.class) //非阻塞模式
+                .option(ChannelOption.AUTO_READ, true)
+                .handler(new MyChannelInitializer());
+            ChannelFuture f = b.connect(host, port).sync();
+            System.out.println("客户端开始链接。。。");
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workGroup.shutdownGracefully();
+        }
+
+    }
+}
